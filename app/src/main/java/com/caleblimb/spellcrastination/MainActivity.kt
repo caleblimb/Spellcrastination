f774f7e3177405c4835e14ecfad8e07f536e53b1
BuildTools <unconfigured@null.spigotmc.org>
2022-05-20 16:59:28 -0600

    Add player count spinner

--- app/src/main/java/com/caleblimb/spellcrastination/MainActivity.kt
+++ app/src/main/java/com/caleblimb/spellcrastination/MainActivity.kt
@@ -6,10 +6,7 @@ import android.text.Editable
 import android.text.InputFilter
 import android.text.TextWatcher
 import android.view.View
-import android.widget.Button
-import android.widget.EditText
-import android.widget.ImageButton
-import android.widget.TextView
+import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
@@ -19,12 +16,31 @@ class MainActivity : AppCompatActivity() {
 
         val startButton = findViewById<Button>(R.id.button_start)
 
+        val playerCountTextView = findViewById<TextView>(R.id.textViewPlayerCount)
+        val playerSpinner = findViewById<Spinner>(R.id.playerSpinner)
+        var playerCount = 2
+
+        if (playerSpinner != null) {
+//            val adapter = ArrayAdapter( this, android.R.layout.simple_spinner_item, playerSpinner)
+//            playerSpinner.adapter = adapter
+            playerSpinner.onItemSelectedListener = object: AdapterView.OnItemSelectedListener {
+                override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                    playerCount = position + 2
+                    playerCountTextView.text = "Player Count ".plus(playerCount.toString())
+                }
+                override fun onNothingSelected(p0: AdapterView<*>?) {
+                    TODO("Not yet implemented")
+                }
+            }
+        }
+
 
         startButton?.setOnClickListener()
         {
-//            val playerCount = findViewById<EditText>(R.id.editTextNumberPlayerCount).text as Int
-//            val game : Game = Game(playerCount)
+
             val intent = Intent(this@MainActivity, Game::class.java)
+
+//            intent.putExtra("PLAYER_COUNT", playerCount)
             startActivity(intent)
         }
     }
