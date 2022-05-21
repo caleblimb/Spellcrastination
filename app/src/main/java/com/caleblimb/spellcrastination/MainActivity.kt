1cbb8a892d360d1403b0a1df78e2288b6af2db34
Caleb Limb <caleblimb@gmail.com>
2022-05-20 18:20:36 -0600

    Pass Players with Intent

--- app/src/main/java/com/caleblimb/spellcrastination/MainActivity.kt
+++ app/src/main/java/com/caleblimb/spellcrastination/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.caleblimb.spellcrastination
 
 import android.content.Intent
+import android.graphics.Color
 import android.os.Bundle
 import android.text.Editable
 import android.text.InputFilter
@@ -8,6 +9,9 @@ import android.text.TextWatcher
 import android.view.View
 import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintLayout
+import android.widget.EditText
+import androidx.core.view.get
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,15 +23,39 @@ class MainActivity : AppCompatActivity() {
         val playerCountTextView = findViewById<TextView>(R.id.textViewPlayerCount)
         val playerSpinner = findViewById<Spinner>(R.id.playerSpinner)
         var playerCount = 2
+        val playersView = findViewById<LinearLayout>(R.id.playersView)
+        val context = this
 
         if (playerSpinner != null) {
 //            val adapter = ArrayAdapter( this, android.R.layout.simple_spinner_item, playerSpinner)
 //            playerSpinner.adapter = adapter
-            playerSpinner.onItemSelectedListener = object: AdapterView.OnItemSelectedListener {
-                override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+            playerSpinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+                override fun onItemSelected(
+                    parent: AdapterView<*>?,
+                    view: View?,
+                    position: Int,
+                    id: Long
+                ) {
                     playerCount = position + 2
-                    playerCountTextView.text = "Player Count ".plus(playerCount.toString())
+                    playerCountTextView.text = "Players ".plus(playerCount.toString())
+                    if (playerCount > playersView.childCount) {
+                        while (playerCount > playersView.childCount) {
+                            val player: EditText = EditText(context)
+                            player.hint = "Player Name"
+//                            player.setBackgroundColor(Color.BLUE)
+                            player.layoutParams = ConstraintLayout.LayoutParams(
+                                ConstraintLayout.LayoutParams.MATCH_PARENT,
+                                96
+                            )
+                            playersView.addView(player)
+                        }
+                    } else if (playerCount < playersView.childCount) {
+                        while (playerCount < playersView.childCount) {
+                            playersView.removeViewAt(playersView.childCount - 1)
+                        }
+                    }
                 }
+
                 override fun onNothingSelected(p0: AdapterView<*>?) {
                     TODO("Not yet implemented")
                 }
@@ -37,10 +65,13 @@ class MainActivity : AppCompatActivity() {
 
         startButton?.setOnClickListener()
         {
-
             val intent = Intent(this@MainActivity, Game::class.java)
-
-//            intent.putExtra("PLAYER_COUNT", playerCount)
+            val players: MutableList<String> = ArrayList<String>()
+            for (p in 0 until playerCount) {
+                val np = playersView[p] as EditText
+                players.add(np.text.toString())
+            }
+            intent.putStringArrayListExtra("PLAYERS", players as ArrayList<String>)
             startActivity(intent)
         }
     }
